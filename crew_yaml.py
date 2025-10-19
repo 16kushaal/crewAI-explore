dfb91a0d4062785d7485177511823dc9e801d7a6
16kushaal <kushaal.1607@gmail.com>
2025-10-19 23:52:04 +0530

    Added YAML Exec

---
+++ crew_yaml.py
@@ -0,0 +1,53 @@
+from crewai import Agent, Crew, Task
+from crewai.project import CrewBase, agent, task, crew
+from crewai_tools import SerperDevTool
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+@CrewBase
+class BlogCrew():
+    '''Blog Writing Crew with Researcher and Writer Agents'''
+    agents_config = "config/agents.yaml"
+    tasks_config = "config/tasks.yaml"
+
+    @agent
+    def researcher(self) -> Agent:
+        return Agent(
+            config=self.agents_config['researcher'],
+            tools=[SerperDevTool()],
+            verbose=True
+        )
+    
+    @agent
+    def writer(self) -> Agent:
+        return Agent(
+            config=self.agents_config['writer'],
+            verbose=True
+        )
+
+    @task
+    def research_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['research_task']
+        )
+    
+    @task
+    def write_task(self) -> Task:
+        return Task(
+            config=self.tasks_config['write_task']
+        )
+    
+    @crew
+    def crew(self) -> Crew:
+        return Crew(
+            agents=[self.researcher(), self.writer()],
+            tasks=[self.research_task(), self.write_task()],
+            verbose=True
+        )
+
+if __name__ == "__main__":
+    blog_crew = BlogCrew()
+    topic = input("Enter the blog topic: ")
+    blog_crew.crew().kickoff(inputs={"topic":f"{topic}"})
